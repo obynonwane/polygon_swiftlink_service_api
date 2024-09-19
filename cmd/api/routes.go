f6db8aa4ea62f408dc8d376a31616f74c1c39059
obynonwane <biostechnologyng@gmail.com>
2024-09-19 10:31:17 +0100

    initial commit

---
+++ cmd/api/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+/* returns http.Handler*/
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	//specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/api/v1/getusers", app.GetUsers)
+	// Add the Prometheus metrics endpoint to the router
+	mux.Handle("/metrics", promhttp.Handler())
+
+	return mux
+}
